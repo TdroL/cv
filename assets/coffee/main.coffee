7b8b74822fde0aab47e55fad0b3c6d4f2b9d970e
tdroL <stdrol@gmail.com>
2012-09-03 23:02:59 +0200

    rewrite of switchPosition plugin

--- assets/coffee/main.coffee
+++ assets/coffee/main.coffee
@@ -2,25 +2,23 @@ $ ->
 
 	$navListItems = $('nav li')
 	$navLinks = $navListItems.find('a')
+	$areas = $('.area')
 
 	selectors = ("a[href^=\"##{link.id}\"]" for link in $navLinks)
 
-	activateArea = (area) ->
-		$area = $(area).closest '.area'
+	$(document).on 'click', selectors.join(','), (e, areasState = 'back', areaState = 'front') ->
+		$area = $(@hash).closest '.area'
 
-		$area.switchPosition('recall')
-			.siblings('.area').switchPosition 'send'
+		$areas.switchPosition areasState
+		$area.switchPosition areaState
+
+		console.log 'click'
 
 		$navListItems.removeClass 'active'
 		$navLinks.filter("a[href='##{$area.prop('id')}']")
 			.closest('li').addClass 'active'
 
-	$(document).on 'click', selectors.join(','), (e) ->
-		activateArea @hash
-
 		if Modernizr.mq 'only screen and (min-width: 980px)'
 			e.preventDefault()
 
-	$('.area').addClass 'enable-animations'
-
-	$navLinks.first().trigger 'click'
+	$navLinks.first().trigger 'click', ['back-flash', 'front-flash']
--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Avoid `console` errors in browsers that lack a console.
 if (!(window.console && console.log)) {
 	(function() {
@@ -13,61 +15,173 @@ if (!(window.console && console.log)) {
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
-$(document).on('webkitTransitionEnd msTransitionEnd oTransitionEnd', '*', function (e) {
-	$(this).trigger('transitionend');
-});
-
 // position switcher
+(function ($, doc) {
 
-$.fn.switchPosition = function (action) {
+	var transEndEventNames = {
+	    	'WebkitTransition' : 'webkitTransitionEnd',
+	    	'MozTransition'    : 'transitionend',
+	    	'OTransition'      : 'oTransitionEnd',
+	    	'msTransition'     : 'MSTransitionEnd',
+	    	'transition'       : 'transitionend'
+	    },
+	    transitionName = transEndEventNames[ Modernizr.prefixed('transition') ];
 
-	if (action == 'send' || action == 'send-flash') {
-		// if any element is in middle of transition, trigger "transitionend" event to skip animations
-		this.trigger('transitionend.switchPosition');
+	// simple transition handler/helper
+	var transition = {
+		isActive: function ($el) {
+			// check if transform changed (isn't "none") - this function return false
+			// for browsers without translate3d support or with small screen
+			var transform = $el.css('transform');
+			return transform && transform != 'none';
+		},
+		transitionCheckTimeOut: 12,
+		on: function ($el, callback) {
+			// attach element to transitionend event
+			$el.on(transitionName + '.switchPosition', function () {
+				// at the end of transition execute the callback
+				callback();
+			});
 
-		// send each element to the background
-		return this.each(function () {
-			var $this = $(this),
-			    positionId;
+			// create delayed check for active transition
+			var timer = setTimeout(function () {
+				if ( ! transition.isActive($el)) {
+					// if no transition is active or browser does not support it, execute
+					// the callback manually
+					callback();
+				}
+			}, transition.transitionCheckTimeOut);
 
-			// get target position for this element
-			positionId = 'position-' + $this.data('position');
+			// remember timer id
+			$el.data('timer.switchPosition', timer);
+		},
+		off: function ($el) {
+			// detach element from transitionend event
+			$el.off(transitionName + '.switchPosition');
 
-			// start the "send" animation
-			$this.addClass(positionId);
+			// disable timer if it is still active
+			var timer = $el.data('timer.switchPosition');
+			if (timer)
+			{
+				clearTimeout(timer);
+				$el.removeData('timer.switchPosition');
+			}
+		}
+	};
 
-			// skip elements already in the background
-			if ( ! $this.is('.in-background')) {
-				// attach "transitionend" event for animated element
-				$this.one('transitionend.switchPosition', function () {
-					// at the end of transition add class "in-background" to prevent from attaching "transitionend" event
-					$this.addClass('in-background');
-				});
+	// "state machine"
+	var state = {
+		// main function for managing states of provided element
+		request: function ($el, newState) {
+			// get name of current state, if the element has any
+			var curState = $el.data('state');
 
-				// if requested "send-flash" method, trigger "transitionend" event immediately
-				if (action == 'send-flash') {
-					$this.trigger('transitionend.switchPosition');
+			// validate current state name
+			if (curState != null && this.states.hasOwnProperty(curState))
+			{
+				// execute method "release" for current state - detach and/or remove
+				// any elements and handlers created by method "init" of this state
+				this.states[curState].release.call($el);
+			}
+
+			// validate new state name
+			if (newState != null  && this.states.hasOwnProperty(newState))
+			{
+				// initialize new state for element
+				this.states[newState].init.call($el);
+			}
+
+			// remember new state name
+			$el.data('state', newState);
+		},
+		states: {
+			// state for elements in the foreground
+			'front': {
+				init: function () {},
+				release: function () {}
+			},
+			// state for elements in the background
+			'back': {
+				init: function () {
+					// add "position-*" and "in-background" classes to sent the element
+					// to proper  place in the background
+					var position = 'position-' + this.data('position');
+
+					this.addClass(position + ' in-background');
+				},
+				release: function () {
+					// remove "position-*" and "in-background" to recall the element from
+					// the background, and (depending of the next state) start transition
+					var position = 'position-' + this.data('position');
+
+					this.removeClass(position + ' in-background');
+				}
+			},
+			// state for elements in transition from the background to the foreground
+			'move-to-front': {
+				init: function () {
+					var self = this;
+
+					// after end of the transition, switch state of the element to "front"
+					// animation and transition is activated in method "release" of state
+					// "back"
+					transition.on(this, function () {
+						state.request(self, 'front');
+					});
+				},
+				release: function () {
+					// detach the element from event transitionend
+					transition.off(this);
+				}
+			},
+			// state for elements in transition from the foreground to the background
+			'move-to-back': {
+				init: function () {
+					var position = 'position-' + this.data('position'),
+					    self = this;
+
+					// start the transition by adding class "position-*" to the element
+					this.addClass(position);
+					// after end of the transition, switch state of the element to "back"
+					transition.on(this, function () {
+						state.request(self, 'back');
+					});
+				},
+				release: function () {
+					var position = 'position-' + this.data('position');
+
+					// remove "position-*" class from the element - this class will be
+					// readded in state "back"
+					this.removeClass(position);
+					// detach the element from event transitionend
+					transition.off(this);
 				}
 			}
-		});
-	}
+		}
+	};
 
-	if (action == 'recall') {
-		// if any element is in middle of transition, trigger "transitionend" event to skip animations
-		this.trigger('transitionend.switchPosition');
+	$.fn.switchPosition = function (reqState) {
+		var rstate = /^(front|back)(-flash)?$/i;
 
-		// recall each element from the background
+		// validate requested state name
+		if ( ! rstate.test(reqState)) {
+			console.error('$#switchPosition: Invalid state %s, avaible: front, back, front-flash, back-flash', reqState);
+			return this;
+		}
+
 		return this.each(function () {
 			var $this = $(this),
-			    positionId;
+			    curState = $this.data('state');
 
-			// get target position for this element
-			positionId = 'position-' + $this.data('position');
+			// if requested "flash" switch, then skip transitional "move-to-*" states
+			if (reqState.indexOf('flash') != -1) {
+				state.request($this, reqState.replace(rstate, '$1'));
 
-			if ($this.hasClass(positionId)) {
-				// remove "position-*" and "in-background" classes from this element - start the "recall" animation
-				$this.removeClass(positionId + ' in-background');
+			// otherwise request transitional state "move-to-*"
+			} else if (reqState !== curState) {
+				state.request($this, 'move-to-' + reqState);
 			}
 		});
-	}
-};
+	};
+
+})($, document);
